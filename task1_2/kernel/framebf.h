59d950000e386161dd86ce45efa456a1c82567e6
LanaJ0 <poiqwep123@gmail.com>
2023-09-14 14:24:45 +0700

    task2 update

--- task1_2/kernel/framebf.h
+++ task1_2/kernel/framebf.h
@@ -7,4 +7,4 @@ void drawRectARGB32(int x1, int y1, int x2, int y2, unsigned int attr, int fill)
 void printFont(unsigned char *fontData, int x, int y, unsigned int color0, unsigned int color1, int pixelSize);
 void printString(const char *str, int x, int y, unsigned int color0, unsigned int color1, int pixelSize);
 unsigned char *getFontDataForCharacter(char character);
-unsigned char fontData[53 * 8];
+unsigned char fontData[63 * 8];
--- task1_2/kernel/kernel.c
+++ task1_2/kernel/kernel.c
@@ -13,9 +13,12 @@ void main()
     framebf_init();
 
     printString("Jaeheon Jeong", 50, 50, 0, 0x00AA0000, 4);
-    printString("Sanghwa Jung", 50, 100, 0, 0x0000BB00, 4);
-    printString("Le Thanh Tai", 50, 150, 0x000000CC, 1, 4);
-    printString("Vo Hoang Tuan", 50, 200, 0x000000CC, 0x00FFFF00, 4);
+    // printString("Sanghwa Jung", 50, 100, 0, 0x0000BB00, 4);
+    // printString("Le Thanh Tai", 50, 150, 0x000000CC, 1, 4);
+    // printString("Vo Hoang Tuan", 50, 200, 0x000000CC, 0x00FFFF00, 4);
+
+    printString("0123456789", 50, 100, 0, 0x00AA0000, 4);
+
 
     // echo everything back
     while(1) {
